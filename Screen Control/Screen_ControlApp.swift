353545aed3229cd0c79648c850abc138a1b0f4ab
iSAD <bradx2713@gmail.com>
2024-06-23 10:46:57 +0200

    Initial Commit

---
+++ Screen Control/Screen_ControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  Screen_ControlApp.swift
+//  Screen Control
+//
+//  Created by Tafadzwa Alexander on 2024/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Screen_ControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
